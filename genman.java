1c929f8ea2f70009ff7507f8e341c54ebd8c3c6a
Tegar Mandiri <TegarMandiri@users.noreply.github.com>
2019-06-11 18:30:07 +0700

    Update genman.java
    - Renamed Variable

--- genman.java
+++ genman.java
@@ -1,5 +1,7 @@
 //Finished, will added more features++
-//Ver. 1.00
+//Ver. 1.25
+//Credit Tegar Mandiri 2019
+//Contact email: tegarmndr@gmail.com
 
 import java.util.Scanner;
 import java.util.Random;
@@ -8,40 +10,46 @@ public class genman{
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
 
-//Credit Tegar Mandiri 2019
-//Contact email: tegarmndr@gmail.com
-
 		System.out.println(" ");
 		System.out.println(" ");
 		System.out.println("//Copyright Tegar Mandiri 2019");
-		System.out.println("-Ver. 1.00");
+		System.out.println("-Ver. 1.25");
 		System.out.println(" ");
 		System.out.println("--------------- Generator Mandiri (GENMAN) ---------------");
 		System.out.println(" ");
 
 		System.out.print("Enter how many names 	: ");
-		int nama = Integer.parseInt(scan.nextLine());
+		int names = Integer.parseInt(scan.nextLine());
 
-		String janama[] = new String[nama];
-		for (int i = 0; i < janama.length; i++){
-			System.out.print("Name 	" + (i+1) + " : ");
-			janama[i] = scan.nextLine();
-		}
+		String allNames [] = new String [names];
+
+			for (int i = 0; i < allNames.length; i++) {
+			
+				System.out.print("Name 	" + (i+1) + " : ");
+				allNames[i] = scan.nextLine();
+			
+			}
 
 		System.out.println(" ");
 		System.out.println("Press Enter key to generate...");
-		try
-		{
-			System.in.read();
-        }  
-        catch(Exception e)
-        {}
+
+			try {
+
+				System.in.read();
+    
+        	}  
+
+        		catch(Exception e) {
+
+        	}
 
 		System.out.println("------------------------------------");
 
-		Random a = new Random();
-		int randoma = a.nextInt(janama.length);
-		System.out.println("Result name = " + janama[randoma]);
+		Random random = new Random();
+		int resultRandom = random.nextInt(allNames.length);
+		System.out.println("Result name = " + allNames[resultRandom]);	
+
 		System.out.println(" ");
+
 	}
 }
